eff3a565b2c4666ac26e93736387d3dab867a3b8
Rakeshh2604 <vayigandlarakesh@gmail.com>
2025-10-13 19:26:46 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,4 @@
+print('hello world')
+# there are two types of modules in python built in and external 
+# modules. Bulit in are the ones that are already there and external needs to be downloaded
+print(5+5)
